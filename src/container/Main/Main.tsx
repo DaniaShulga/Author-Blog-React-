4eea4dc2dd91bd6abd662901b7403f8557525262
Dania <27072003danya@gmail.com>
2023-03-05 21:14:49 +0200

    add logo

--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -1,5 +1,21 @@
+import './Main.scss'
+import './Reset.scss'
+import header_logo from 'assets/header_logo.png'
+
 type Props = {}
 const Main = (props: Props) => {
-    return <div>Main</div>
+    return (
+        <main className="main">
+            <div className="container">
+                <div className="logo_wrapper">
+                    <div className="logo_img">
+                        <a href="#">
+                            <img src={header_logo} alt="header logo" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
 }
 export default Main
