564e43a8d989bb32565f784a447d75563978bb40
Dania <27072003danya@gmail.com>
2023-03-20 14:01:28 +0200

    add new changes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import Articles from 'components/Articles/Articles'
 import InterestingInformation from 'components/InterestingInformation/InterestingInformation'
 import Logo from 'components/Logo/Logo'
 import NewsLetter from 'components/NewsLetter/NewsLetter'
+import Purchase from 'components/Purchase/Purchase'
 
 type Props = {}
 const Home = (props: Props) => {
@@ -11,6 +12,7 @@ const Home = (props: Props) => {
             <InterestingInformation />
             <Articles />
             <NewsLetter />
+            <Purchase />
         </div>
     )
 }
