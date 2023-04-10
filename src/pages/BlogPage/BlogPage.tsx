41f2c469389f3b0badddf815a52ce8b5d6811a2c
Dania <27072003danya@gmail.com>
2023-04-11 01:04:36 +0300

    add reviews form

--- src/pages/BlogPage/BlogPage.tsx
+++ src/pages/BlogPage/BlogPage.tsx
@@ -4,6 +4,8 @@ import { useParams } from 'react-router-dom'
 import articlesArray, { Props, getArticlesObject } from 'utils/articlesArray'
 import CommentsImg from 'assets/comment_img.png'
 import './BlogPage.scss'
+import { useState } from 'react'
+import arrReviews, { Review } from 'utils/arrReviews'
 
 type Prop = {
     articlesObject?: {
@@ -16,7 +18,64 @@ const BlogPage = ({
     const { id } = useParams()
 
     const article = articlesObject[parseInt(id!)]
-    console.log(article)
+
+    const [reviews, setReviews] = useState<Review[]>(arrReviews)
+    const [newReview, setNewReview] = useState<Review>({
+        name: '',
+        eMail: '',
+        website: '',
+        comment: '',
+    })
+
+    const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewReview((prevState) => ({
+            ...prevState,
+            name: e.target.value,
+        }))
+    }
+
+    const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewReview((prevState) => ({
+            ...prevState,
+            eMail: e.target.value,
+        }))
+    }
+
+    const handleWebsite = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewReview((prevState) => ({
+            ...prevState,
+            website: e.target.value,
+        }))
+    }
+
+    const handleComment = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setNewReview((prevState) => ({
+            ...prevState,
+            comment: e.target.value,
+        }))
+    }
+
+    const onSend = (e: React.ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (
+            newReview.name === '' ||
+            newReview.eMail === '' ||
+            newReview.website === '' ||
+            newReview.comment === ''
+        ) {
+            alert('All fields are required!')
+        } else {
+            setReviews((prevState) => {
+                return [...prevState, newReview]
+            })
+            setNewReview({
+                name: '',
+                eMail: '',
+                website: '',
+                comment: '',
+            })
+        }
+    }
 
     return (
         <>
@@ -51,46 +110,66 @@ const BlogPage = ({
                                 <div className="comments_count">
                                     <p>1 Comment</p>
                                 </div>
-                                <div className="comments">
-                                    <div className="comments_img">
-                                        <img src={CommentsImg} alt="" />
-                                    </div>
-                                    <div className="comments_inf">
-                                        <p className="comments_user">admin</p>
-                                        <p className="comments_data">
-                                            POSTED AT 09:45H, 28 FEBRUARY
-                                        </p>
-                                        <p className="comments_text">
-                                            Sit ei hendrerit voluptatum.
-                                        </p>
-                                    </div>
-                                </div>
+
+                                {reviews.map(
+                                    (
+                                        { name, eMail, website, comment },
+                                        idx
+                                    ) => (
+                                        <div className="comments" key={idx}>
+                                            <div className="comments_img">
+                                                <img src={CommentsImg} alt="" />
+                                            </div>
+                                            <div className="comments_inf">
+                                                <p className="comments_user">
+                                                    {name}
+                                                </p>
+                                                <p className="comments_data">
+                                                    {eMail} | {website}
+                                                </p>
+                                                <p className="comments_text">
+                                                    {comment}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    )
+                                )}
                             </div>
-                            <div className="comment_write">
+                            <form className="comment_write" onSubmit={onSend}>
                                 <label>Post A Comment</label>
-                                <textarea placeholder="Write your comments here..."></textarea>
+                                <textarea
+                                    placeholder="Write your comments here..."
+                                    value={newReview.comment}
+                                    onChange={handleComment}
+                                ></textarea>
                                 <input
                                     type="text"
                                     placeholder="Your full name"
                                     className="user_name"
+                                    value={newReview.name}
+                                    onChange={handleName}
                                 />
                                 <input
                                     type="email"
                                     placeholder="E-mail address"
                                     className="user_email"
+                                    value={newReview.eMail}
+                                    onChange={handleEmail}
                                 />
                                 <input
                                     type="text"
                                     placeholder="Website"
                                     className="user_website"
+                                    value={newReview.website}
+                                    onChange={handleWebsite}
                                 />
-                                <label>
+                                <label className="comment_checkbox">
                                     <input type="checkbox" />
                                     Save my name, email, and website in this
                                     browser for the next time I comment.
                                 </label>
-                                <button>submit</button>
-                            </div>
+                                <button type="submit">submit</button>
+                            </form>
                         </div>
                         <Aside />
                     </div>
---
+++ src/utils/arrReviews.ts
@@ -0,0 +1,17 @@
+export type Review = {
+    name:string
+    eMail:string
+    website:string
+    comment:string
+}
+
+export const arrReviews: Review[] = [
+    {
+        name:"Евгеній",
+        eMail:"evgenii@gmail.com",
+        website:"blog resourse",
+        comment:"Навіть не знаю, щр написати. Телефон як телефон. Не був моєю мрією ніколи)) Перейшов з Huawei P20 якому вдже 5-й рік ішов. Перший ифон. Ніякого вау ефекта не викликав. Сподобалась батарея. В моїх режимах використання тримає майже 3 дні",
+    },
+]
+
+export default arrReviews;
